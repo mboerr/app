f500116babee3320f3d4ccc66d74c55eecb97647
Mateo Boerr <mateo.boerr@gmail.com>
2020-05-03 20:49:20 -0300

    version 2

---
+++ app/app.cpp
@@ -0,0 +1,14 @@
+// app.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "HI THERE\n";
+
+	std::getchar();
+    return 0;
+}
+
